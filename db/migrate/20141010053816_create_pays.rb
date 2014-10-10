320d3dff9e71934a33e44f72d24665178b92db8a
dachin <zzlove2006@gmail.com>
2014-10-10 13:46:45 +0800

    第一次commit

---
+++ db/migrate/20141010053816_create_pays.rb
@@ -0,0 +1,12 @@
+class CreatePays < ActiveRecord::Migration
+  def change
+    create_table :pays do |t|
+      t.string :date
+      t.string :payer
+      t.integer :money
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
