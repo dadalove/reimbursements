7d28c840a680753625101d70eb0fdd3968b2137d
dachin <zzlove2006@gmail.com>
2014-10-22 12:55:25 +0800

    code review by ihower

--- app/controllers/pays_controller.rb
+++ app/controllers/pays_controller.rb
@@ -1,16 +1,18 @@
 class PaysController < ApplicationController
+
   def index
-    @pays=Pay.all
-    @pays = Pay.page(params[:page]).per(5)
+    @pays = Pay.order("id DESC").page(params[:page]).per(5)
   end
 
   def new
-    @pay=Pay.new
+    @pay = Pay.new
   end
 
   def create
-    @pay=Pay.new(pay_params)
-    @pay.status = "false"
+    @pay = Pay.new(pay_params)
+
+    @pay.status = "not_paid"
+
     if @pay.save
       redirect_to pays_path
     else 
@@ -19,15 +21,18 @@ class PaysController < ApplicationController
   end
 
   def update
-    @pay=Pay.find(params[:id])
-    @pay.status = "true"
+    @pay = Pay.find(params[:id])
+
+    @pay.status = "paid"
     @pay.save
-    redirect_to pays_path
+
+    redirect_to pays_path( :page => params[:page] )
   end
 
   def destroy
-    @pay=Pay.find(params[:id])
+    @pay = Pay.find(params[:id])
     @pay.destroy
+
     redirect_to pays_path( :page => params[:page] )
   end
 
--- app/models/pay.rb
+++ app/models/pay.rb
@@ -1,4 +1,11 @@
 class Pay < ActiveRecord::Base
+
   validates :date, :money, presence: true
   validates :money, numericality: {only_integer: true, greater_than: 0}
+
+
+  def is_paid?
+    status == "paid"
+  end
+
 end
